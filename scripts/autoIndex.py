12bc538c40fb4ccd0c15395b1f8a7e534f297e26
phaesler <paul.haesler@data61.csiro.au>
2019-05-20 16:12:36 +0100

    STAC scan implemented.  Still need to index them.

--- scripts/autoIndex.py
+++ scripts/autoIndex.py
@@ -6,69 +6,56 @@ import click
 import logging
 import json
 
+from satstac import Catalog, Collection, Item
+from satsearch import Search
+
 logger = logging.getLogger('odcindexer')
 
-def convert_ll_to_pr(extent, ascending, path):
+def stac_search(extent, start_date, end_date):
     """ Convert lat, lon to pathrows """
-    logger.info("Starting the conversion from ll to pathrow for area: {}".format(extent))
-    driver = ogr.GetDriverByName('ESRI Shapefile')
-    file_path = '/vsizip/' + path 
-    dataSource = driver.Open(file_path, 0) # 0 means read-only. 1 means writeable.
-    if not dataSource:
-        logger.error("Failed to open the file: {}".format(file_path))
-        return
-    layer = dataSource.GetLayer()
-
-    ring = ogr.Geometry(ogr.wkbLinearRing)
-    # Stupid geometry is lat, lon instead of lon, lat...
-    ring.AddPoint(extent[0], extent[2])
-    ring.AddPoint(extent[0], extent[3])
-    ring.AddPoint(extent[1], extent[3])
-    ring.AddPoint(extent[1], extent[2])
-    ring.AddPoint(extent[0], extent[2])
-    poly = ogr.Geometry(ogr.wkbPolygon)
-    poly.AddGeometry(ring)
-
-    logging.info("Usion bbox filter: {}".format(poly.ExportToJson()))
-    layer.SetSpatialFilter(poly)
-
-    if not ascending:
-        layer.SetAttributeFilter("MODE = 'A'")
-    else:
-        layer.SetAttributeFilter("MODE = 'D'")
+    logger.info("Querying STAC for area: {} and times: {} - {} (UTC)".format(extent, start_date, end_date))
+    srch = Search(bbox=extent, time='{}T00:00:00Z/{}T23:59:59Z'.format(start_date,end_date), 
+                    url="https://sat-api.developmentseed.org/stac/search")
+    try:
+        logger.info("Found {} items".format(srch.found()))
+        return srch
+    except KeyError:
+        return None
 
 
-    logging.info("Found {} features.".format(layer.GetFeatureCount()))
-    pathRows = []
-    for pInfo in layer:
-        pathRows.append([pInfo.GetField('PATH'), pInfo.GetField('ROW')])
+def index_datasets(items):
+    for item in items:
+        if "MTL" in item.assets:
+            logger.info("TODO: index {}".format(item.assets["MTL"]["href"]))
+        else:
+            logger.warn("WARN: Item {} does not have an MTL asset (Sentinel2?) - skipping".format(item))
 
-    return pathRows
 
 # Probably should use Click like the other scripts? -agl
 @click.command()
 @click.option('--extents', '-e', default="146.30,146.83,-43.54,-43.20", help="Extent to index in the form lon_min,lon_max,lat_min,latmax")
-@click.option('--pathrow_file', '-p', default="/opt/odc/data/wrs2_descending.zip", help="Absolute path to the pathrow file, e.g., /tmp/example.zip")
-@click.option('--start_date', default="2013-02-11", help="Start date of the acquisitions to index, in YYYY-MM-DD format")
-@click.option('--end_date', default="2099-12-31", help="End date of the acquisitions to index, in YYYY-MM-DD format")
-def index(extents, pathrow_file, start_date, end_date, write_extents=True):
+# @click.option('--pathrow_file', '-p', default="/opt/odc/data/wrs2_descending.zip", help="Absolute path to the pathrow file, e.g., /tmp/example.zip")
+@click.option('--start_date', default="2013-02-11", help="Start date of the acquisitions to index, in YYYY-MM-DD format (UTC)")
+@click.option('--end_date', default="2099-12-31", help="End date of the acquisitions to index, in YYYY-MM-DD format (UTC)")
+def index(extents, start_date, end_date, write_extents=True):
     lon_min, lon_max, lat_min, lat_max = map(float, extents.split(','))
 
     if write_extents:
         with open('/opt/odc/data/configIndex.txt', 'w') as outfile:
             json.dump({'extent': [lon_min, lon_max, lat_min, lat_max]}, outfile)
 
-    pathRows = convert_ll_to_pr([lon_min, lon_max, lat_min, lat_max], True, pathrow_file)
-    if not pathRows:
-        logging.error("Couldn't find pathrows, stopping.")
+    srch = stac_search([lon_min, lat_min, lon_max, lat_max], start_date, end_date)
+    if not srch:
+        logging.error("STAC search failed, stopping.")
         return
-    logging.info("Found {} pathrows to handle".format((len(pathRows))))
-    for pathRow in pathRows:
-        logging.info("Loading pathrows")
-        print(pathRow)
-        os.system('python3 ./ls_public_bucket.py landsat-pds -p c1/L8/' + "%03d" % (pathRow[0],) + '/' + "%03d" % (pathRow[1],) + '/ --suffix="MTL.txt"' + ' --start_date %s' % start_date + ' --end_date %s' % end_date)
+    logging.info("Indexing datasets...")
+    index_datasets(srch.items())
+    logging.info("And we're done!")
+
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
     logger.info("Starting the index process")
     index()
+
+
