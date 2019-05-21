ff4c8076668dfb43a192e931c8e50064fc7049ec
phaesler <paul.haesler@data61.csiro.au>
2019-05-21 12:37:16 +0100

    Multiprocessing support.

--- scripts/autoIndex.py
+++ scripts/autoIndex.py
@@ -9,13 +9,16 @@ import boto3
 import datacube
 from urllib.parse import urlparse
 
-# from  multiprocessing import Process, current_process, Queue, Manager, cpu_count
+from  multiprocessing import Process, current_process, Queue, Manager, cpu_count
+from queue import Empty
 
 from ls_public_bucket import _parse_group, make_metadata_doc, get_s3_url, add_dataset
 
 
 from satstac import Catalog, Collection, Item
 from satsearch import Search
+
+STOP_SIGN = "ALL_COMPLETE_END_WORKER"
 
 logger = logging.getLogger('odcindexer')
 
@@ -31,30 +34,81 @@ def stac_search(extent, start_date, end_date):
         return None
 
 
-def index_datasets(items):
+def index_dataset(index, s3, url, parse_only):
+    logger.info("Downloading {}".format(url))
+    bucket_name, key = parse_s3_url(url)
+    obj = s3.Object(bucket_name, key).get(ResponseCacheControl='no-cache')
+    raw = obj['Body'].read()
+    raw_string = raw.decode('utf8')
+    logger.info("Parsing {}".format(key))
+    try:
+        txt_doc = _parse_group(iter(raw_string.split("\n")))['L1_METADATA_FILE']
+        data = make_metadata_doc(txt_doc, bucket_name, key)
+    except Exception as e:
+        logger.error("Metadata parsing error: {}; {}".format(e.__class__.__name__, e))
+        return
+    uri = get_s3_url(bucket_name, key)
+    cdt = data['creation_dt']
+    if parse_only:
+        logger.info("Skipping indexing step")
+    else:
+        logger.info("Indexing {}".format(key))
+        add_dataset(data, uri, idx, "verify")
+
+
+def index_datasets(items, parse_only=False):
     s3 = boto3.resource("s3")
     dc = datacube.Datacube()
     idx = dc.index
     for item in items:
         if "MTL" in item.assets:
-            logger.info("Downloading {}".format(item.assets["MTL"]["href"]))
-            bucket_name, key = parse_s3_url(item.assets["MTL"]["href"])
-            obj = s3.Object(bucket_name, key).get(ResponseCacheControl='no-cache')
-            raw = obj['Body'].read()
-            raw_string = raw.decode('utf8')
-            logger.info("Parsing {}".format(key))
-            try:
-                txt_doc = _parse_group(iter(raw_string.split("\n")))['L1_METADATA_FILE']
-                data = make_metadata_doc(txt_doc, bucket_name, key)
-            except Exception as e:
-                logger.error("Metadata parsing error: {}; {}".format(e.__class__.__name__, e))
-            uri = get_s3_url(bucket_name, key)
-            cdt = data['creation_dt']
-            logger.info("Indexing {}".format(key))
-            add_dataset(data, uri, idx, "verify")
+            index_dataset(idx, s3, item.assets["MTL"]["href"], parse_only)
         else:
             logger.info("Item {} does not have an MTL asset (Sentinel2?) - skipping".format(item))
 
+
+def worker(parse_only, queue):
+    s3 = boto3.resource("s3")
+    dc = datacube.Datacube()
+    idx = dc.index
+
+    while True:
+        try:
+            url = queue.get(timeout=60)
+            if url == STOP_SIGN:
+                break
+            logging.info("Processing {} {}".format(url, current_process()))
+            index_dataset(idx, s3, url, parse_only)
+            queue.task_done()
+        except Empty:
+            break
+        except EOFError:
+            break
+
+
+def index_datasets_multi(items, parse_only=False):
+    manager = Manager()
+    queue = manager.Queue()
+    worker_count = cpu_count() * 2
+    
+    processes = []
+    for i in range(worker_count):
+        proc = Process(target=worker, args=[parse_only, queue])
+        processes.append(proc)
+        proc.start()
+
+    for item in items:
+        if "MTL" in item.assets:
+            queue.put(item.assets["MTL"]["href"])
+        else:
+            logger.info("Item {} does not have an MTL asset (Sentinel2?) - skipping".format(item))
+
+    for i in range(worker_count):
+        queue.put(STOP_SIGN)
+
+    for proc in processes:
+        proc.join()
+    
 
 def parse_s3_url(url):
     o = urlparse(url)
@@ -74,10 +128,12 @@ def parse_s3_url(url):
 # @click.option('--pathrow_file', '-p', default="/opt/odc/data/wrs2_descending.zip", help="Absolute path to the pathrow file, e.g., /tmp/example.zip")
 @click.option('--start_date', default="2013-02-11", help="Start date of the acquisitions to index, in YYYY-MM-DD format (UTC)")
 @click.option('--end_date', default="2099-12-31", help="End date of the acquisitions to index, in YYYY-MM-DD format (UTC)")
-def index(extents, start_date, end_date, write_extents=True):
+@click.option('--single_process_only', is_flag=True, help="If true, multi-processing is disabled")
+@click.option('--parse_only', '-p', is_flag=True, help="If true, scan STAC and parse MTL files, but do not index into datacube")
+def index(extents, start_date, end_date, single_process_only, parse_only, write_extents=True):
     lon_min, lon_max, lat_min, lat_max = map(float, extents.split(','))
 
-    if write_extents:
+    if write_extents and not parse_only:
         with open('/opt/odc/data/configIndex.txt', 'w') as outfile:
             json.dump({'extent': [lon_min, lon_max, lat_min, lat_max]}, outfile)
 
@@ -86,7 +142,10 @@ def index(extents, start_date, end_date, write_extents=True):
         logging.error("STAC search failed, stopping.")
         return
     logging.info("Indexing datasets...")
-    index_datasets(srch.items())
+    if single_process_only:
+        index_datasets(srch.items(), parse_only)
+    else:
+        index_datasets_multi(srch.items(), parse_only)
     logging.info("And we're done!")
 
 
